09a2c4ba8ff234cb8cea1442eba4b67ddb9e56f5
Colin Brayton <gringolalia@gmail.com>
2020-01-16 17:53:31 -0300

    deploy

---
+++ precache-manifest.1036f3102413a0448bb8e4bf53dbfc55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f146b192b8fd6c65f26a",
+    "url": "/operation-bomber-velocity/css/app.15000f1f.css"
+  },
+  {
+    "revision": "bb630acc3430a009648a",
+    "url": "/operation-bomber-velocity/css/chunk-vendors.feced395.css"
+  },
+  {
+    "revision": "89f0cfd0dfa7fb685406cb08a04aa01b",
+    "url": "/operation-bomber-velocity/img/biohazard.89f0cfd0.svg"
+  },
+  {
+    "revision": "2c974997e0fca648ac8dbf5dd6fe7705",
+    "url": "/operation-bomber-velocity/index.html"
+  },
+  {
+    "revision": "77b255694103db0cc715",
+    "url": "/operation-bomber-velocity/js/about.a3173253.js"
+  },
+  {
+    "revision": "f146b192b8fd6c65f26a",
+    "url": "/operation-bomber-velocity/js/app.1fd075fd.js"
+  },
+  {
+    "revision": "bb630acc3430a009648a",
+    "url": "/operation-bomber-velocity/js/chunk-vendors.5395bdb6.js"
+  },
+  {
+    "revision": "f18aaee0076657edd7185c174942e709",
+    "url": "/operation-bomber-velocity/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/operation-bomber-velocity/robots.txt"
+  }
+]);
